e950435c3f1afe15da737d4b7c0b571e3e6c6f4e
chanukya1986 <chanukya2410@gmail.com>
2024-06-29 12:09:58 +0530

    commit done

---
+++ test.py
@@ -0,0 +1,25 @@
+# Write the addition of two numbers
+number1 = float(input("Enter number1:"))
+number2 = float(input("Enter number2:"))
+sum = number1+number2
+print(f"sum of the {number1} and {number2} are :{sum}")
+
+# Write the difference of two numbers
+number1 = float(input("Enter number1:"))
+number2 = float(input("Enter number2:"))
+Difference = number1-number2
+print(f"Difference of the {number1} and {number2} are :{Difference}")
+
+# Write the multiplication of two numbers
+number1 = float(input("Enter number1:"))
+number2 = float(input("Enter number2:"))
+Multiplication = number1*number2
+print(f"Multiplication of the {number1} and {number2} are :{Multiplication}")
+
+# Division of the two numbers
+number1 = float(input("Enter number1:"))
+number2 = float(input("Enter number2:"))
+Division = number1/number2
+print(f"Division of the {number1} and {number2} are :{Division}")
+
+
